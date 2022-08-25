6c4116101e7271b7fd2a04f41b795ac8a2154f80
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2022-08-25 11:27:57 +0900

    항상 마지막에 코드 실행

--- dist/index.js
+++ dist/index.js
@@ -1,84 +1,101 @@
+/**
+ * Copyright (c) 2022 DE:MO
+ * MIT License
+ * Github : https://github.com/demooss/undefined
+ */
 (()=>{
-    /**
-     * Body 데이터 값 저장
-     */
-    let body = document.querySelector('body').innerHTML;
-
-    /**
-     * 데이터 바인딩
-     * @returns 바인딩된 데이터 값
-     */
-    const App = () => {
-        let ret = body;
-        body.match(/\{\{([^}]+)\}\}/g).forEach((e) => {
-            const classNameChars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
-            var className = "";
-            for (let i = 0; i < 8; i++) {
-            const rnum = Math.floor(Math.random() * classNameChars.length)
-            className += classNameChars.substring(rnum, rnum + 1)
-            };
-
-            let t = e;
-
-            // input Value 받아옴
-            document.querySelectorAll('[bind-value]').forEach((v)=>{
-                if(v.value){
-                    if(isNaN(v.value)){
-                        t = t.replaceAll(v.getAttribute('bind-value'),  `\"${v.value}\"`);
-                    }else{
-                        t = t.replaceAll(v.getAttribute('bind-value'),  v.value);
-                    }
-                }else{
-                    if(v.type == "number"){
-                        t = t.replaceAll(v.getAttribute('bind-value'),  '0');
-                    }else{
-                        t = t.replaceAll(v.getAttribute('bind-value'),  '"Null"');
-                    }
-                }
-            });
-            t = t.replaceAll('{','');
-            t = t.replaceAll('}','');
-            t =  new Function(`return ${t}`)();
-
-            // input이 변경될때마다 실행
-            window.addEventListener('input',()=>{
-                let m = e;
-                let attributeValue = "";
+    addEventListener('load',()=>{
+        /**
+         * Body 데이터 값 저장
+         */
+        let body = document.querySelector('body').innerHTML;
+    
+        /**
+         * 데이터 바인딩
+         * @returns 바인딩된 데이터 값
+         */
+        const App = () => {
+            let ret = body;
+            // {{}} 문법으로 작성된 부분을 찾음
+            body.match(/\{\{([^}]+)\}\}/g).forEach((e) => {
+                const classNameChars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+                var className = "";
+                for (let i = 0; i < 8; i++) {
+                const rnum = Math.floor(Math.random() * classNameChars.length)
+                className += classNameChars.substring(rnum, rnum + 1)
+                };
+    
+                // 데이터가 꼬이지않도록 t로 복제
+                let t = e;
+    
                 // input Value 받아옴
-                m = m.replaceAll('{','');
-                m = m.replaceAll('}','');
                 document.querySelectorAll('[bind-value]').forEach((v)=>{
-                    attributeValue = v.getAttribute('bind-value');
-                    if(m.matchAll(attributeValue)){
-                        if(v.value){
-                            if(isNaN(v.value)){
-                                m = m.replaceAll(attributeValue,  `\"${v.value}\"`);
-                            }else{
-                                m = m.replaceAll(attributeValue,  v.value);
-                            }
+                    if(v.value){
+                        // 숫자인지 판단
+                        if(isNaN(v.value)){
+                            t = t.replaceAll(v.getAttribute('bind-value'),  `\"${v.value}\"`);
                         }else{
-                            if(v.type == "number"){
-                                m = m.replaceAll(v.getAttribute('bind-value'),  '0');
-                            }else{
-                                m = m.replaceAll(v.getAttribute('bind-value'),  '"Null"');
-                            }
+                            t = t.replaceAll(v.getAttribute('bind-value'),  v.value);
+                        }
+                    }else{
+                        // 인풋의 타입이 number 일때 데이터가 없을경우 0으로 설정
+                        if(v.type == "number"){
+                            t = t.replaceAll(v.getAttribute('bind-value'),  '0');
+                        }else{
+                            // 그 외 타입은 데이터가 없을경우 Null
+                            t = t.replaceAll(v.getAttribute('bind-value'),  '"Null"');
                         }
                     }
                 });
-                m = new Function(`return ${m}`)();
-                if(document.querySelector(`.${className}`).innerHTML != m){
-                    document.querySelector(`.${className}`).innerHTML = m;  
+                t = t.replaceAll('{','');
+                t = t.replaceAll('}','');
+                t = new Function(`return ${t}`)();
+    
+                // input이 변경될때마다 실행
+                window.addEventListener('input',()=>{
+                    let m = e;
+                    let attributeValue = "";
+                    // 데이터 바인딩 문법에서 {{}}를 지움
+                    m = m.replaceAll('{','');
+                    m = m.replaceAll('}','');
+    
+                    // input Value 받아옴
+                    document.querySelectorAll('[bind-value]').forEach((v)=>{
+                        attributeValue = v.getAttribute('bind-value');
+                        if(m.matchAll(attributeValue)){
+                            if(v.value){
+                                // 숫자인지 판단
+                                if(isNaN(v.value)){
+                                    m = m.replaceAll(attributeValue,  `\"${v.value}\"`);
+                                }else{
+                                    m = m.replaceAll(attributeValue,  v.value);
+                                }
+                            }else{
+                                // 인풋의 타입이 number 일 경우 초기값은 0으로 설정
+                                if(v.type == "number"){
+                                    m = m.replaceAll(v.getAttribute('bind-value'),  '0');
+                                }else{
+                                    // 그 외 타입은 데이터가 없을경우 Null
+                                    m = m.replaceAll(v.getAttribute('bind-value'),  '"Null"');
+                                }
+                            }
+                        }
+                    });
+                    m = new Function(`return ${m}`)();
+                    if(document.querySelector(`.${className}`).innerHTML != m){
+                        document.querySelector(`.${className}`).innerHTML = m;  
+                    }
+                });
+    
+                if(t != undefined){
+                    ret = ret.replaceAll(e,`<span class="${className}">${t}</span>`);
+                }else{
+                    ret = ret.replaceAll(e,``);
                 }
             });
-
-            if(t != undefined){
-                ret = ret.replaceAll(e,`<span class="${className}">${t}</span>`);
-            }else{
-                ret = ret.replaceAll(e,``);
-            }
-        });
-        return ret;
-    };
-
-    document.querySelector('body').innerHTML = App();
+            return ret;
+        };
+    
+        document.querySelector('body').innerHTML = App();
+    })
 })();
